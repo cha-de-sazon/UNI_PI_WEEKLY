54ba53aad7fbfc3d7a667030c5992a34793036f3
cha-de-sazon <104666188+cha-de-sazon@users.noreply.github.com>
2025-02-27 23:05:11 -0300

    Add files via upload

---
+++ ep1_2.py
@@ -0,0 +1,12 @@
+# input 1 = 2
+# input 2 = 1.41421
+# input 3 = 3.14159
+
+n1 = int(input("input 1"))
+print(n1)
+
+n2 = float(input("input 2"))
+print(round(n2, 2))
+
+n3 = float(input("input 3"))
+print(round(n3, 3))
