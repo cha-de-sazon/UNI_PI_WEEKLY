3326d53383ace1432fc9fae57170e07f189ac48f
cha-de-sazon <104666188+cha-de-sazon@users.noreply.github.com>
2025-03-03 20:58:47 -0300

    Delete ep1_3.py

--- ep1_3.py
+++
@@ -1,5 +0,0 @@
-# Exercício: Escreva uma função/método com o nome somar que recebe dois valores inteiros (a e b).
-# Essa função/método deve retornar o valor da soma.
-
-def somar(a, b):
-    return(a + b)
