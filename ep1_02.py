7919f444118efeb166bd93afd98cb94776caa5bb
cha-de-sazon <104666188+cha-de-sazon@users.noreply.github.com>
2025-03-18 00:51:11 -0300

    Update ep1_02.py

--- ep1_02.py
+++ ep1_02.py
@@ -1,8 +1,8 @@
-n1 = int(input("Primeiro Número:"))
+n1 = int(input("Primeiro Número: "))
 print(f"Primeiro numero = {n1}")
 
-n2 = float(input("Segundo Número:"))
+n2 = float(input("Segundo Número: "))
 print(f"{n2:.2f} eh o segundo numero")
 
-n3 = float(input("Terceiro Número:"))
+n3 = float(input("Terceiro Número: "))
 print(f"Finalmente {n3:.3f} eh o terceiro numero")
